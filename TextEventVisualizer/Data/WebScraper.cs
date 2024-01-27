45d8aef79bf878ce055e72aa7a0d6eda3f1b3d15
Atle Aarrestad <atleaarrestad@gmail.com>
2024-01-27 19:47:00 +0100

    added tab component and basic webscrape test

---
+++ TextEventVisualizer/Data/WebScraper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace TextEventVisualizer.Data
+{
+    public static class WebScraper
+    {
+
+        public static async Task<string> Scrape()
+        {
+            string url = "https://www.huffpost.com/entry/russian-controlled-ukrainian-regions-referendum_n_6329d53ae4b07198f012f023";
+            string htmlClassCriteria = "primary-cli cli cli-text ";
+
+            HttpClient httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var desiredNodes = htmlDoc.DocumentNode.SelectNodes($".//div[@class='{htmlClassCriteria}']");
+            foreach ( var desiredNode in desiredNodes )
+            {
+                Console.WriteLine(desiredNode.InnerText);
+            }
+
+            return "";
+        }
+
+    }
+}
