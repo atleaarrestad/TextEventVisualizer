cb9c8d3be3e2cac32a06ee99ebd349fb266760a3
Atle Aarrestad <atleaarrestad@gmail.com>
2024-01-31 20:43:47 +0100

    article indexes to improve queries on category and date

--- TextEventVisualizer/Data/ApplicationDbContext.cs
+++ TextEventVisualizer/Data/ApplicationDbContext.cs
@@ -5,8 +5,17 @@ namespace TextEventVisualizer.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Article> Articles { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-        public DbSet<Article> Articles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Article>()
+                .HasIndex(a => a.Category);
+
+            modelBuilder.Entity<Article>()
+                .HasIndex(a => a.Date);
+        }
     }
 }
---
+++ TextEventVisualizer/Migrations/20240131194218_article_index.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TextEventVisualizer.Migrations
+{
+    /// <inheritdoc />
+    public partial class article_index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_Category",
+                table: "Articles",
+                column: "Category");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_Date",
+                table: "Articles",
+                column: "Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_Category",
+                table: "Articles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_Date",
+                table: "Articles");
+        }
+    }
+}
