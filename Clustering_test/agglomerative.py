3d7f8882e3dfa8a3c9b63e0c0011a9de65edc650
RealStig <greb54321@gmail.com>
2024-02-07 14:51:27 +0100

    .

--- Clustering_test/agglomerative.py
+++ Clustering_test/agglomerative.py
@@ -9,19 +9,6 @@ embedder = SentenceTransformer("all-MiniLM-L6-v2")
 
 nlp = spacy.load("en_core_web_sm")
 
-# try: 
-    # Establish a connection to the SQLite database
-    # conn = sqlite3.connect(db_path)
-    # print("Connected to the database")
-
-    # cursor = conn.cursor()
-
-#     cursor.execute('SELECT * FROM Articles LIMIT 1')
-#     row = cursor.fetchone()
-#     print("Sample query executed successfully:", row)
-
-# except sqlite3.Error as e:
-#     print("Error connecting to the database:", e)
 
 conn = sqlite3.connect(db_path)
 print("Connected to the database")
