f1d0654542a5b90b0f21d7537fc5b4d515fb6876
Atle Aarrestad <atleaarrestad@gmail.com>
2024-03-13 21:47:54 +0100

    removed initial blazor tutorial pages and some unrelated python code

--- TextEventVisualizer/Program.cs
+++ TextEventVisualizer/Program.cs
@@ -12,7 +12,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlite("Data Source=database.db");
